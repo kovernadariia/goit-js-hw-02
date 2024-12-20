250d5e6d55dab6d6c2f1783d6c7cc38076f5c574
Dariia <dkoverna4@gmail.com>
2024-12-20 18:18:17 +0100

    s

--- js/task-3.js
+++ js/task-3.js
@@ -1,19 +1,3 @@
-function getElementWidth(content, padding, border) {
-    // Перетворення рядкових значень в числа
-    const contentWidth = parseFloat(content); // Ширина контенту
-    const paddingWidth = parseFloat(padding); // Горизонтальний падінг
-    const borderWidth = parseFloat(border); // Товщина бордера
-    
-    // Враховуємо падінг та бордер для загальної ширини
-    const totalWidth = contentWidth + 2 * (paddingWidth + borderWidth); // Падінг та бордер на обох сторонах
-    
-    return totalWidth; // Повертаємо загальну ширину
-}
-
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
-
 function checkForSpam(message) {
     const newMessage = message.toLowerCase();
     if (newMessage.includes('spam') || newMessage.includes('sale')) {
@@ -22,10 +6,10 @@ function checkForSpam(message) {
         return false;
     }
 }
-console.log(checkForSpam("Latest technology news")); // false
-console.log(checkForSpam("JavaScript weekly newsletter")); // false
-console.log(checkForSpam("Get best sale offers now!")); // true
-console.log(checkForSpam("Amazing SalE, only tonight!")); // true
-console.log(checkForSpam("Trust me, this is not a spam message")); // true
-console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
-console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
+console.log(checkForSpam("Latest technology news")); 
+console.log(checkForSpam("JavaScript weekly newsletter")); 
+console.log(checkForSpam("Get best sale offers now!")); 
+console.log(checkForSpam("Amazing SalE, only tonight!")); 
+console.log(checkForSpam("Trust me, this is not a spam message")); 
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); 
+console.log(checkForSpam("[SPAM] How to earn fast money?")); 
